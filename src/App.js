6e0f43303b83c77d28d8ed8dc6a17b17ce74972f
hoathang9695 <61120953+hoathang9695@users.noreply.github.com>
2022-03-31 11:01:05 +0700

    test infiniti scoll

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import axios from "axios";
 
 export default function App() {
+  const [items, setItems] = React.useState([]);
+  const fetchMoreData = async () => {
+    let configs = {
+      url: `https://demo.treblle.com/api/v1/articles`,
+      method: "get"
+    };
+
+    let response = await axios(configs);
+    setItems(items.concat(response.data.articles));
+    return response;
+  };
+  React.useEffect(() => {
+    fetchMoreData();
+  }, []);
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App example
         </Typography>
-        <ProTip />
-        <Copyright />
+
+        <div id="scrollableDiv" style={{ height: 600, overflow: "auto" }}>
+          <InfiniteScroll
+            dataLength={items.length}
+            next={fetchMoreData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            scrollableTarget="scrollableDiv"
+          >
+            {items.map((i, index) => (
+              <Card key={"card" + index} sx={{ maxWidth: 645 }}>
+                <CardActionArea>
+                  <div key={index}>div - #{index}</div>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={i.image}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {i.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {i.content}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </InfiniteScroll>
+        </div>
       </Box>
     </Container>
   );
